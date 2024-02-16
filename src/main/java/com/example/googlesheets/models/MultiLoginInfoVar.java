72d664c6594eb3ba7ac9f299e4196c2ede6de239
Sh1bari <vova_krasnov_2004@mail.ru>
2024-02-16 13:19:41 +0300

    Evgeniy done

--- src/main/java/com/example/googlesheets/models/MultiLoginInfoVar.java
+++ src/main/java/com/example/googlesheets/models/MultiLoginInfoVar.java
@@ -15,11 +15,13 @@ public class MultiLoginInfoVar {
     private String token;
     private String spent;
     private String creativeId;
+    private String campaignName;
 
-    public MultiLoginInfoVar(String account, String token, String creativeId, String spent) {
+    public MultiLoginInfoVar(String account, String token, String creativeId, String spent, String campaignName) {
         this.account = account;
         this.token = token;
         this.creativeId = creativeId;
         this.spent = spent;
+        this.campaignName = campaignName;
     }
 }
